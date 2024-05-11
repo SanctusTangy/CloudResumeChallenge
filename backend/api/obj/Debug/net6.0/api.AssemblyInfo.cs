62d44b03749f8c34c258fa4e37834fafb920df83
Kyle Harris <sanctuseagle@comcast.net>
2024-05-11 14:54:27 -0400

    update

--- backend/api/obj/Debug/net6.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad90c3846666ee7ea40d9904c0e69836ac57eb00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e200f31c77b48bd9c8d6ed6fcb194690e0bb30a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
